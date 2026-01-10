d2a602eb383ce2c78a67aca4c71dd14a723b0cd2
Yukko31415 <yukko31415@gmail.com>
2026-01-10 18:44:47 +0900

    途中で空行を出力するように変更

--- src/main.lisp
+++ src/main.lisp
@@ -10,5 +10,6 @@
   "Entry point for the application."
   (format t "ファイルパスを入力: ")
   (finish-output)
-  (wikilinter-parser:destruct-ftml-block
-   (uiop:read-file-string (uiop:parse-native-namestring (read-line)))))
+  (let ((textdata (uiop:read-file-string (uiop:parse-native-namestring (read-line)))))
+    (terpri)
+    (wikilinter-parser:destruct-ftml-block textdata)))
