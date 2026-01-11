576e58ed048ae1accd4d2d2f832541711b77d2ee
Yukko31415 <yukko31415@gmail.com>
2026-01-11 23:14:11 +0900

    ファイルが見つからなかった時のエラー処理を追加

--- src/main.lisp
+++ src/main.lisp
@@ -2,15 +2,24 @@
 ;;;
 ;;; SPDX-License-Identifier: MIT
 ;;;
-;;; Copyright (C) 2025 Your Name
+;;; Copyright (C) 2025 Yukko
+
 
 (in-package #:wikidot-linter)
 
-(defun main ()
-  "Entry point for the application."
+(defun get-string-from-file ()
   (format t "ファイルパスを入力: ")
   (finish-output)
-  (let ((textdata (uiop:read-file-string (uiop:parse-native-namestring (read-line)))))
+  (handler-case
+      (uiop:read-file-string (uiop:parse-native-namestring (read-line)))
+    (sb-ext:file-does-not-exist ()
+      (format t "ファイルが見つかりませんでした。もう一度入力してください。~%")
+      (finish-output)
+      (get-string-from-file))))
+
+(defun main ()
+  "Entry point for the application."
+  (let ((textdata (get-string-from-file)))
     (terpri)
     (time (wikilinter-parser:destruct-ftml-block textdata)))
   (loop
