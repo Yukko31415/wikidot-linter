ae4fae9fa93828a952cd2bc1c76758a366882a06
Yukko31415 <yukko31415@gmail.com>
2026-01-12 15:53:45 +0900

    関数をモジュール化

--- src/main.lisp
+++ src/main.lisp
@@ -14,25 +14,31 @@
   (format t "ファイルパスを入力: ")
   (finish-output)
   (handler-case
-      (uiop:read-file-string (uiop:parse-native-namestring (read-line)))
+      (prog1 (uiop:read-file-string (uiop:parse-native-namestring (read-line)))
+	(terpri))
     (sb-ext:file-does-not-exist ()
       (format t "ファイルが見つかりませんでした。もう一度入力してください。~%")
       (finish-output)
       (get-string-from-file))))
 
+(defun get-string-and-destruct-ftml ()
+  (let ((textdata (get-string-from-file)))
+    (time (wikilinter-parser:destruct-ftml-block textdata))
+    (format t "qで終了/nで次のファイル~%")
+    (finish-output)))
 
 
 (defun main ()
   "Entry point for the application."
   (loop
-    :for textdata := (get-string-from-file)
+    :initially (get-string-and-destruct-ftml)
 
-    :do (terpri)
-	(time (wikilinter-parser:destruct-ftml-block textdata))
-	(format t "qで終了/nで次のファイル~%")
-	(finish-output)
-	(alexandria:switch ((read-line) :test #'string=)
-	  ("q" (loop-finish))
-	  ("n" nil))
-    :finally (format t "終了します. . .")))
+    :for command := (read-line)
+
+    :until (string= "q" command)
+
+    :when (string= "n" command)
+      :do (get-string-and-destruct-ftml)
+
+    :finally (format t "終了します...")))
 
