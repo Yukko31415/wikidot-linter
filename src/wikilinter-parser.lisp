f2a3d3586a7a2781bfda8ef0c55c6ec1f7f034a3
Yukko31415 <yukko31415@gmail.com>
2026-03-07 14:11:17 +0900

    微修正

--- src/wikilinter-parser.lisp
+++ src/wikilinter-parser.lisp
@@ -14,89 +14,6 @@
 			    :displaced-index-offset start))
 
 
-;;
-;; ftml-refs
-
-(declaim (ftype (function (integer parsed-ftml) (values integer integer))
-		ftml-ref-line ftml-ref-loc))
-
-(defun ftml-ref-loc (index parsed-ftml)
-  (with-slots (ftml-location ftml-length ftml-string) parsed-ftml
-    (when (> ftml-length index)
-      (let* ((start (aref ftml-location index))
-	     (end (aref ftml-location (1+ index))))
-	(values start end)))))
-
-(defun ftml-ref-line (index parsed-ftml)
-  (with-slots (ftml-length ftml-line) parsed-ftml
-    (when (> ftml-length index)
-      (values (aref ftml-line index)
-	      (aref ftml-line (1+ index))))))
-
-
-(defun ftml-ref (index parsed-ftml)
-  (with-slots (ftml-location ftml-length ftml-string) parsed-ftml
-    (when (> ftml-length index)
-      (multiple-value-bind (start end) (ftml-ref-loc index parsed-ftml)
-	(duplicate-string ftml-string :start start :end end)))))
-
-
-
-
-(defun ftml-header (parsed-ftml)
-  (with-slots (ftml-location ftml-string) parsed-ftml
-    (unless (zerop (aref ftml-location 0))
-      (let ((end (aref ftml-location 0)))
-	(duplicate-string ftml-string :start 0 :end end)))))
-
-
-
-;; --------------------------------------------------
-;; get-tag-and-params
-;; --------------------------------------------------
-
-
-(defun get-block-range-of (text &key (start 0) (end (length text)))
-  "コードブロックとそれ以外の文字列を分ける"
-  (bind ((scanner (load-time-value (ppcre:create-scanner "(?s)(?<!\\[)\\[\\[(?!\\[).*?(?<!\\])\\]\\](?!\\])")))
-	 ((:values match-start match-end) (ppcre:scan scanner text :start start :end end)))
-    (values match-start match-end)))
-
-(defun get-all-whitespace-blocs (text &key (start 0) (end (length text)))
-  (let ((scanner (load-time-value (ppcre:create-scanner "[ \\n\\t\\r]+"))))
-    (ppcre:all-matches scanner text :start start :end end)))
-
-
-(defun get-block-range-of/with-parsed-ftml (index parsed-ftml)
-  (bind (((:values start end) (ftml-ref-loc index parsed-ftml))
-	 ((:values match-start match-end)
-	  (get-block-range-of (ftml-string parsed-ftml) :start start :end end)))
-    (values start end match-start match-end)))
-
-
-
-
-
-
-(defun get-tag-and-params (index parsed-ftml)
-  (bind (((:slots ftml-string) parsed-ftml)
-	 ((:values _ end match-start match-end)
-	  (get-block-range-of/with-parsed-ftml index parsed-ftml))
-	 ((&optional tag &rest loc-list)
-	  (get-all-whitespace-blocs (ftml-string parsed-ftml) :start match-start :end match-end)))
-    (values (duplicate-string ftml-string :start (+ 2 match-start)
-					  :end (or tag (- match-end 2)))
-	    (loop :for (start end) :on loc-list :by #'cddr
-		  :if end :collect (duplicate-string ftml-string :start start
-								 :end end)
-		    :else :if (> (- match-end 2) start)
-			    :collect (duplicate-string ftml-string :start start
-								   :end (- match-end 2))
-		  :end
-		  :while end)
-	    (duplicate-string ftml-string :start match-end :end end))))
-
-
 
 ;; --------------------------------------------------
 ;; parse-ftml-text
@@ -117,14 +34,13 @@
 			     :fill-pointer 0)))
     (when offsets (loop :for i :in offsets :by #'cddr
 			:do (vector-push i result)
+			:count i :into length
 			:finally (vector-push (length text) result)
-				 (return result)))))
+				 (return (values result length))))))
 
 (defun %count-lines (str start end)
   (declare (type simple-string str))
-  (if (equal str "") 0
-      (count #\Newline str :start start :end end)))
-
+  (count #\Newline str :start start :end end))
 
 (defun count-lines (str loc &aux (line 0) (start 0))
   (flet ((f (end) (prog1 (incf line (%count-lines str start end))
@@ -133,11 +49,99 @@
 
 
 (defun parse-ftml-text (string)
-  (let ((loc (make-loc-list string)))
+  (bind (((:values loc length) (make-loc-list string)))
     (%make-parsed-ftml :ftml-string string
 		       :ftml-location loc
 		       :ftml-line (count-lines string loc)
-		       :ftml-length (1- (length loc)))))
+		       :ftml-length length)))
+
+
+
+
+;;
+;; ftml-refs
+
+(declaim (ftype (function (integer parsed-ftml) (values integer integer))
+		ftml-ref-line ftml-ref-loc))
+
+(defun ftml-ref-loc (index parsed-ftml)
+  (with-slots (ftml-location ftml-length ftml-string) parsed-ftml
+    (when (> ftml-length index)
+      (let* ((start (aref ftml-location index))
+	     (end (aref ftml-location (1+ index))))
+	(values start end)))))
+
+(defun ftml-ref-line (index parsed-ftml)
+  (with-slots (ftml-length ftml-line) parsed-ftml
+    (when (> ftml-length index)
+      (values (aref ftml-line index)
+	      (aref ftml-line (1+ index))))))
+
+
+
+
+(defun ftml-ref (index parsed-ftml)
+  (with-slots (ftml-location ftml-length ftml-string) parsed-ftml
+    (when (> ftml-length index)
+      (multiple-value-bind (start end) (ftml-ref-loc index parsed-ftml)
+	(duplicate-string ftml-string :start start :end end)))))
+
+
+
+(defun ftml-header (parsed-ftml)
+  (with-slots (ftml-location ftml-string) parsed-ftml
+    (unless (zerop (aref ftml-location 0))
+      (let ((end (aref ftml-location 0)))
+	(duplicate-string ftml-string :start 0 :end end)))))
+
+
+
+
+;; --------------------------------------------------
+;; get-tag-and-params
+;; --------------------------------------------------
+
+
+
+(defun get-block-range-of (text &key (start 0) (end (length text)))
+  "コードブロックとそれ以外の文字列を分ける"
+  (bind ((scanner (load-time-value (ppcre:create-scanner "(?s)(?<!\\[)\\[\\[(?!\\[).*?(?<!\\])\\]\\](?!\\])")))
+	 ((:values match-start match-end) (ppcre:scan scanner text :start start :end end)))
+    (values match-start match-end)))
+
+
+(defun get-all-whitespace-blocs (text &key (start 0) (end (length text)))
+  (let ((scanner (load-time-value (ppcre:create-scanner "[ \\n\\t\\r]+"))))
+    (ppcre:all-matches scanner text :start start :end end)))
+
+
+
+
+
+
+(defun get-block-range-of/with-parsed-ftml (index parsed-ftml)
+  (bind (((:values start end) (ftml-ref-loc index parsed-ftml))
+	 ((:values match-start match-end)
+	  (get-block-range-of (ftml-string parsed-ftml) :start start :end end)))
+    (values start end match-start match-end)))
+
+(defun get-tag-and-params (index parsed-ftml)
+  (bind (((:slots ftml-string) parsed-ftml)
+	 ((:values _ end match-start match-end)
+	  (get-block-range-of/with-parsed-ftml index parsed-ftml))
+	 ((&optional tag &rest loc-list)
+	  (get-all-whitespace-blocs (ftml-string parsed-ftml) :start match-start :end match-end)))
+    
+    (values (duplicate-string ftml-string :start (+ 2 match-start)
+					  :end (or tag (- match-end 2)))
+	    (loop :for (start end) :on loc-list :by #'cddr
+		  :if end :collect (duplicate-string ftml-string :start start :end end)
+		    :else :if (> (- match-end 2) start)
+			    :collect (duplicate-string ftml-string :start start
+								   :end (- match-end 2))
+		  :end
+		  :while end)
+	    (duplicate-string ftml-string :start match-end :end end))))
 
 
 
@@ -226,7 +230,8 @@
 			      (ignore () (push-contents component content outer))))))
 	      (setf counter crr-loc))))
 
-(defmethod %%destruct-ftml-block ((component components:unclassified) other parsed-ftml crr-loc)
+(defmethod %%destruct-ftml-block ((component components:unclassified)
+				  other parsed-ftml crr-loc)
   (values component other crr-loc))
 
 
@@ -253,13 +258,13 @@
 ;; --------------------------------------------------
 
 
-(defun destruct-ftml-block (string &aux (parsed-ftml (parse-ftml-text string))
-				     (log nil))
+(defun destruct-ftml-block (string &key (stream t) &aux (parsed-ftml (parse-ftml-text string))
+						     (log nil))
   (declare (type simple-string string))
   (handler-bind ((parse-time-log #'(lambda (c) (push (log-condition c) log)))
 		 (invalid-end-tag-name #'(lambda (c) (push c log) (invoke-restart 'ignore))))
     (prog1 (destruct-ftml-block/toplevel parsed-ftml)
-      (format t "~{~A~%~}" (sort log #'(lambda (a b) (< (crr-line a) (crr-line b))))))))
+      (format stream "~{~A~%~}" (sort log #'(lambda (a b) (< (crr-line a) (crr-line b))))))))
 
 
 
